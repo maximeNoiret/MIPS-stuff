fcfdc07387464a40d9cf388130c89e25bdcc8d5f
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2025-05-10 21:36:34 +0200

    Made power function better
    -Removed pseudo instructions
    -Saved arguments to stack

--- libMath.asm
+++ libMath.asm
@@ -11,19 +11,26 @@
 # Note:
 #     Complexity of O(log2(n)), aka logarithmic, aka good, much better than the other one
 power:
-	stra
+	addi	$sp,	$sp,	-8		# allocate 2 words in stack (don't save $a0 since never modified)
+	sw		$ra,	4($sp)			# store return address for recursion
+	sw		$a1,	($sp)			# store power argument
 	# if $a1 == 1, return $a0
 	bne		$a1,	1,	notOne
-	move	$v0,	$a0
-	return
+	or		$v0,	$zero,	$a0
+	lw		$ra,	4($sp)			# retrieve current return address
+	addi	$sp,	$sp,	8		# deallocate 2 words in stack
+	jr		$ra						# return $a0
 	# else,
 	notOne:
 	andi	$t0,	$a1,	1		# get $a1 modulo 2 to know if it's even or odd
 	bnez	$t0,	odd				# if the power is even,
 	srl		$a1,	$a1,	1		# get $a1 / 2
 	jal		power					# z = power(x, n/2)
-	mul		$v0,	$v0,	$v0		# multiply the result by itself
-	return							# return z * z
+	mul		$v0,	$v0,	$v0		# multiply the result by itself (z*z)
+	lw		$a1,	($sp)			# retreive previous power argument
+	lw		$ra,	4($sp)			# retrieve current return address
+	addi	$sp,	$sp,	8		# deallocate 2 words in stack
+	jr		$ra						# return z * z
 	# else, if the power is odd,
 	odd:
 	addi	$a1,	$a1,	-1		# get $a1 - 1
@@ -31,7 +38,10 @@ power:
 	jal		power					# z = power(x, (n-1)/2)
 	mul		$v0,	$v0,	$v0		# z * z
 	mul		$v0,	$a0,	$v0		# then by the number, so basically x * (z * z)
-	return							# return x * z * z
+	lw		$a1,	($sp)			# retreive previous power argument
+	lw		$ra,	4($sp)			# retrieve current return address
+	addi	$sp,	$sp,	8		# deallocate 2 words in stack
+	jr		$ra						# return x * z * z
 
 
 # Function dumbPower
